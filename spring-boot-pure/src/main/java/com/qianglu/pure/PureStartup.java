680d3add36fa71f928ad44bfb0d430e712be3b25
QIANGLU <>
2019-08-14 18:13:10 +0800

    添加纯净版pure项目

---
+++ spring-boot-pure/src/main/java/com/qianglu/pure/PureStartup.java
@@ -0,0 +1,16 @@
+package com.qianglu.pure;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qianglu
+ */
+@SpringBootApplication
+public class PureStartup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PureStartup.class, args);
+	}
+}
