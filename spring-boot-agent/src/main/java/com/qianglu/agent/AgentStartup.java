63a2701a212674657e3838c11ca173141205221f
QIANGLU <>
2019-08-14 18:06:17 +0800

    初始化基础项目

---
+++ spring-boot-agent/src/main/java/com/qianglu/agent/AgentStartup.java
@@ -0,0 +1,16 @@
+package com.qianglu.agent;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qianglu
+ */
+@SpringBootApplication
+public class AgentStartup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentStartup.class, args);
+	}
+}
